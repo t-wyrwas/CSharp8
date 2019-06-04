cffc0eb79f5e52a72763af6b2c421a39b105e317
twyrwas <tomasz.wyrwas@gmail.com>
2019-06-04 19:28:06 +0200

    Initial commit

---
+++ NullableReferences/Program.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable CS0219
+#pragma warning disable CS0168
+using System;
+
+namespace NullableReferences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = null;
+            // string? surname;
+
+            int i = 5;
+            // int j = null;
+
+            int? k = null;
+            Nullable<int> l = null;
+        }
+    }
+}
